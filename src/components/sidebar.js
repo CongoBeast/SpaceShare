3789c2cb075cba2d3ac42ccd0034a496d6dbad3e
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2024-12-30 16:06:11 +0200

    Add files via upload

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../App.css";
@@ -8,7 +8,31 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 const SidebarNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const toggleSidebar = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    // Check if token exists in local storage
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [location]); // Check login status on location change
+
+
+
+  const handleLogout = () => {
+    // Remove token from local storage and update state
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+    navigate('/'); // Redirect to home after logout
+    toggleSidebar(); // Close the sidebar after logout
+  };
 
   return (
     <>
@@ -19,7 +43,7 @@ const SidebarNavbar = () => {
         }`}
       >
         <div className="p-3 text-center">
-          <h4>My App</h4>
+          <h4>SpaceShare</h4>
         </div>
         <div className="nav flex-column nav-pills">
          <Link to="/" className="nav-link text-white">
@@ -27,16 +51,29 @@ const SidebarNavbar = () => {
                 <i className="bi bi-house me-2"></i> Home
             </button>
           </Link>
-          <Link to="/profile" className="nav-link text-white">
-            <button className="btn btn-primary text-white mb-2">
-            <i className="bi bi-person me-2"></i> Profile
-            </button>
-          </Link>
+
           <Link to="/about" className="nav-link text-white">
             <button className="btn btn-primary text-white mb-2">
             <i className="bi bi-gear me-2"></i> About
             </button>
           </Link>
+
+
+          {isLoggedIn && (
+            <>
+            <Link to="/profile" className="nav-link text-white">
+            <button className="btn btn-primary text-white mb-2">
+            <i className="bi bi-person me-2"></i> Profile
+            </button>
+            </Link>
+
+            <Link to="/auth" className="nav-link text-white">
+            <button className="btn btn-primary text-white mb-2" onClick={handleLogout}>
+            <i className="bi bi-gear me-2"></i> Logout
+            </button>
+          </Link>
+            </>
+          )}
          
         </div>
       </div>
