37f311f2c91e2d8dc4e30297eb7aa23314eb5032
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2025-09-21 10:30:20 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,24 @@
-importScripts("https://cdn.pushalert.co/sw-84608.js");
+// importScripts("https://cdn.pushalert.co/sw-84608.js");
+
+self.addEventListener('push', function(event) {
+  const options = {
+    body: event.data ? event.data.text() : 'New message received!',
+    icon: '/icon-192x192.png', // Add your app icon
+    badge: '/badge-72x72.png', // Add your badge icon
+    tag: 'message-notification',
+    data: {
+      url: '/chat' // Where to redirect when notification is clicked
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification('SpaceShare - New Message', options)
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
